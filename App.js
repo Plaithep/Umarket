79409dd477c8807bc4f1faaeaa2ac6d0e02ab497
Plaithep <38246072+Plaithep@users.noreply.github.com>
2020-08-14 05:12:24 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import React from 'react'
+import {createAppContainer,createSwitchNavigator} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import {Ionicons} from "@expo/vector-icons"
+
+
+import LoadingScreen from './screens/LoadingScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import LoginScreen from './screens/LoginScreen'
+
+// For Tabs bar
+import HomeScreen from './screens/HomeScreen'
+import MessagaeScreen from './screens/MessageScreen'
+import PostScreen from './screens/PostScreen'
+import NotificationScreen from './screens/NotificationScreen'
+import ProfileScreen from './screens/ProfileScreen'
+
+import * as firebase from 'firebase'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB3CfSnkhF8mDplcg6iTQ3ucfb0HV3R9wE",
+  authDomain: "umarket-18653.firebaseapp.com",
+  databaseURL: "https://umarket-18653.firebaseio.com",
+  projectId: "umarket-18653",
+  storageBucket: "umarket-18653.appspot.com",
+  messagingSenderId: "320747923282",
+  appId: "1:320747923282:web:d719bcf22b045f15ca3c8b",
+  measurementId: "G-ZTT1LXMFDS"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const AppTabNavigator = createBottomTabNavigator(
+  {
+      Home:{
+        screen: HomeScreen,
+        navigationOptions:{
+          tabBarIcon:({tintColor}) => <Ionicons name="ios-home" size={24} color={tintColor}></Ionicons>
+        }
+      }
+  }
+)
+const AuthStack = createStackNavigator({
+  Login: LoginScreen,
+  Register: RegisterScreen
 });
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading: LoadingScreen,
+      App: AppTabNavigator,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "Loading"
+    }
+  )
+)
---
+++ screens/PostScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import{View,Text,StyleSheet} from "react-native"
+
+export default class PostScreen extends React.Component{
+    render(){
+        return(
+            <View style={StyleSheet.container}>
+                <Text>Message</Text>
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems:"center",
+        justifyContent:"center"
+    }
+})
